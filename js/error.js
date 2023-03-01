8af5bc6d4c480f82113175157b0b7e303891019f
mohima-mimino11 <mohimahoque22@gmail.com>
2023-03-01 17:58:52 +0600

    phone hunter api project done

---
+++ js/error.js
@@ -0,0 +1,69 @@
+// fetch('https://jsonplaceholder.typicode.com/users')
+// .then(res => res.json())
+// .then(data => console.log(data))
+
+// document.getElementById('abc')
+
+// synchronous => works sequentially
+//asynchronous => 
+
+// console.log(1);
+// setTimeout(()=>{
+//     console.log(2);
+// }, 3000);
+// console.log(3);
+
+const person = [
+    {
+     name: "rahim",
+     age: 22,
+     friends: ["rahim,karim,jabbar"],
+    },
+    {
+     name: "rahim2",
+     age: 22,
+     friends: ["rahim,karim,jabbar"],
+    },
+    {
+    name: "rahim3",
+    age: 22,
+    friends: ["rahim,karim,jabbar"],
+   },
+   ];
+
+const element = person.slice(1,2)
+
+//    console.log(element);
+
+const dreamGirl = [
+    {
+     sokina: {
+      name: "bbu",
+      height: "5.4",
+      family: [{ father: "rock", mother: "shila", sister: "chinki" }],
+      age: undefined,
+      contactInfo: [
+       {
+        facebook: {
+         link: "https://www.facebook.com/",
+         followers: "12545",
+         status: "single",
+         friendsList: [{ name: "rofik" }, undefined],
+        },
+       },
+       { instagram: "https://www.instagram.com/" },
+      ],
+     },
+    },
+   ];
+
+// console.log(dreamGirl[0].sokina.contactInfo[1].instagram);
+
+const phones = [
+    { name: "sony", price: 500 },
+    { name: "apple", price: 700 },
+    { name: "sony", price: 700 },
+   ]
+const not500 = phones.filter((phone)=> phone.price != 500);
+
+console.log(not500);
